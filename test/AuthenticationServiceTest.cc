a560acc6e4f77acab528211d2c3559dc05bb5819
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-09-30 18:08:06 +0800

    :sparkles: stub and mock

--- test/AuthenticationServiceTest.cc
+++ test/AuthenticationServiceTest.cc
@@ -1,10 +1,26 @@
 #include "gtest/gtest.h"
 #include "../main/AuthenticationService.h"
 
+class StubRsaTokenDao : public RsaTokenDao {
+public:
+    std::string getRandom(std::string userName) override {
+        return "000000";
+    };
+};
+
+class StubProfileDao : public ProfileDao {
+public:
+    std::string getPassword(std::string userName) override {
+        return "91";
+    };
+};
+
 namespace {
 
     TEST(AuthenticationService, IsValid) {
-        AuthenticationService target;
+        StubProfileDao profileDao;
+        StubRsaTokenDao rsaTokenDao;
+        AuthenticationService target = AuthenticationService(profileDao, rsaTokenDao);
 
         bool actual = target.isValid("joey", "91000000");
 
